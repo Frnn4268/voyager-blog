74ac0d6d846b8c3c06247002a3c865dbc8e75fa4
Frnn4268 <appstutorialesy@gmail.com>
2024-07-24 12:11:26 -0600

    Adding new component called Post.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Post from "./components/Post";
 
 function App() {
   return (
@@ -12,57 +13,9 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
-
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://cdn.shopify.com/s/files/1/1310/3673/files/Best-Car-Blogs-feat.jpg?v=1659583858"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>55 Best Car Blogs for Auto Enthusiasts</h2>
-          <p>
-            If you're on the hunt for the best informative car blogs for all the
-            latest updates in the automotive world, then you're in the right
-            place. Here you'll find the top 55 auto blogs online.
-          </p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://cdn.shopify.com/s/files/1/1310/3673/files/Best-Car-Blogs-feat.jpg?v=1659583858"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>55 Best Car Blogs for Auto Enthusiasts</h2>
-          <p>
-            If you're on the hunt for the best informative car blogs for all the
-            latest updates in the automotive world, then you're in the right
-            place. Here you'll find the top 55 auto blogs online.
-          </p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://cdn.shopify.com/s/files/1/1310/3673/files/Best-Car-Blogs-feat.jpg?v=1659583858"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>55 Best Car Blogs for Auto Enthusiasts</h2>
-          <p>
-            If you're on the hunt for the best informative car blogs for all the
-            latest updates in the automotive world, then you're in the right
-            place. Here you'll find the top 55 auto blogs online.
-          </p>
-        </div>
-      </div>
+      <Post />
+      <Post />
+      <Post />
     </main>
   );
 }
---
+++ client/src/components/Post.jsx
@@ -0,0 +1,26 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img
+          src="https://cdn.shopify.com/s/files/1/1310/3673/files/Best-Car-Blogs-feat.jpg?v=1659583858"
+          alt=""
+        />
+      </div>
+      <div className="texts">
+        <h2>55 Best Car Blogs for Auto Enthusiasts</h2>
+        <p className="info">
+          <a href="" className="author">
+            Fernando Martínez
+          </a>
+          <time datetime="">2024-01-01</time>
+        </p>
+        <p className="summary">
+          If you're on the hunt for the best informative car blogs for all the
+          latest updates in the automotive world, then you're in the right
+          place. Here you'll find the top 55 auto blogs online.
+        </p>
+      </div>
+    </div>
+  );
+}
