12330f43a7f6d3b0ab9059b0b1649ff3650e09dc
Frnn4268 <appstutorialesy@gmail.com>
2024-07-24 11:48:49 -0600

    Adding new changes in App.js and App.css

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          Voyager Blog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <img
+          src="https://cdn.shopify.com/s/files/1/1310/3673/files/Best-Car-Blogs-feat.jpg?v=1659583858"
+          alt=""
+        />
+        <h2>55 Best Car Blogs for Auto Enthusiasts</h2>
+        <p>
+          If you're on the hunt for the best informative car blogs for all the
+          latest updates in the automotive world, then you're in the right
+          place. Here you'll find the top 55 auto blogs online.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
