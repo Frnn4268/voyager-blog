54b0b1efb51a3c09a083f1f83b2afb41ee38d6f1
Frnn4268 <appstutorialesy@gmail.com>
2024-07-24 11:59:56 -0600

    Adding new test changes in App.css and App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,16 +14,54 @@ function App() {
       </header>
 
       <div className="post">
-        <img
-          src="https://cdn.shopify.com/s/files/1/1310/3673/files/Best-Car-Blogs-feat.jpg?v=1659583858"
-          alt=""
-        />
-        <h2>55 Best Car Blogs for Auto Enthusiasts</h2>
-        <p>
-          If you're on the hunt for the best informative car blogs for all the
-          latest updates in the automotive world, then you're in the right
-          place. Here you'll find the top 55 auto blogs online.
-        </p>
+        <div className="image">
+          <img
+            src="https://cdn.shopify.com/s/files/1/1310/3673/files/Best-Car-Blogs-feat.jpg?v=1659583858"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>55 Best Car Blogs for Auto Enthusiasts</h2>
+          <p>
+            If you're on the hunt for the best informative car blogs for all the
+            latest updates in the automotive world, then you're in the right
+            place. Here you'll find the top 55 auto blogs online.
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://cdn.shopify.com/s/files/1/1310/3673/files/Best-Car-Blogs-feat.jpg?v=1659583858"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>55 Best Car Blogs for Auto Enthusiasts</h2>
+          <p>
+            If you're on the hunt for the best informative car blogs for all the
+            latest updates in the automotive world, then you're in the right
+            place. Here you'll find the top 55 auto blogs online.
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://cdn.shopify.com/s/files/1/1310/3673/files/Best-Car-Blogs-feat.jpg?v=1659583858"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>55 Best Car Blogs for Auto Enthusiasts</h2>
+          <p>
+            If you're on the hunt for the best informative car blogs for all the
+            latest updates in the automotive world, then you're in the right
+            place. Here you'll find the top 55 auto blogs online.
+          </p>
+        </div>
       </div>
     </main>
   );
