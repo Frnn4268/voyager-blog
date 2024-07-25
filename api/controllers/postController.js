d3a459a72be5e6cd6fba3949ae4859c5a51d3a4e
Frnn4268 <appstutorialesy@gmail.com>
2024-07-25 15:35:57 -0600

    Adding the Post model and finishing the postController.js

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,11 +1,22 @@
 const fs = require("fs");
 
+const Post = require("../models/Post");
+
 exports.createPost = async (req, res) => {
   const { originalname } = req.file;
   const parts = originalname.split(".");
   const extension = parts[parts.length - 1];
-  const newPath = req.file.path + "." + extension
+  const newPath = req.file.path + "." + extension;
 
-  fs.renameSync(req.file.path, newPath)
-  res.json({ extension });
+  fs.renameSync(req.file.path, newPath);
+
+  const { title, summary, content } = req.body;
+  const postDoc = await Post.create({
+    title,
+    summary,
+    content,
+    cover: newPath,
+  });
+
+  res.json(postDoc);
 };
