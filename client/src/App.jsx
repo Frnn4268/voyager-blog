fc2dbe2b5186137ff8b81a7577cd164e6713bd07
Frnn4268 <appstutorialesy@gmail.com>
2024-07-25 12:31:58 -0600

    Creating new page called CreatePost.jsx and using react-quill

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import CreatePost from "./pages/CreatePost";
 import { UserContextProvider } from "./context/UserContext";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="/create" element={<CreatePost />}></Route>
         </Route>
       </Routes>
     </UserContextProvider>
