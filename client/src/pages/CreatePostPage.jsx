f3d8f5cce75b47449072acea55ddcf5d79c17917
Frnn4268 <appstutorialesy@gmail.com>
2024-07-26 16:05:34 -0600

    Adding new content and handle's in CreatePostPage.jsx

--- client/src/pages/CreatePostPage.jsx
+++ client/src/pages/CreatePostPage.jsx
@@ -1,44 +1,19 @@
 import { useState } from "react";
-import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { Navigate } from "react-router-dom";
-
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Editor from "../components/Editor";
-
-const modules = {
-  toolbar: [
-    [{ header: [1, 2, false] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [
-      { list: "ordered" },
-      { list: "bullet" },
-      { indent: "-1" },
-      { indent: "+1" },
-    ],
-    ["link", "image"],
-    ["clean"],
-  ],
-};
-
-const formats = [
-  "header",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "indent",
-  "link",
-  "image",
-];
 
 export default function CreatePostPage() {
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState("");
+  const [fileName, setFileName] = useState("");
+  const [filePreview, setFilePreview] = useState("");
   const [redirect, setRedirect] = useState(false);
 
   async function createNewPost(ev) {
@@ -65,23 +40,73 @@ export default function CreatePostPage() {
     return <Navigate to="/" />;
   }
 
+  const handleFileChange = (ev) => {
+    const file = ev.target.files[0];
+    setFiles(ev.target.files);
+    setFileName(file?.name || "");
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFilePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setFilePreview("");
+    }
+  };
+
   return (
-    <form onSubmit={createNewPost}>
-      <input
-        type="title"
-        placeholder="Title"
+    <Box component="form" onSubmit={createNewPost} sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+      <Typography variant="h5" component="h1" gutterBottom style={{ fontWeight: 'bold' }}>
+        Create a New Post
+      </Typography>
+      <TextField
+        label="Title"
+        variant="outlined"
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
-      <input
-        type="summary"
-        placeholder="Summary"
+      <TextField
+        label="Summary"
+        variant="outlined"
         value={summary}
         onChange={(ev) => setSummary(ev.target.value)}
       />
-      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
-      <Editor onChange={setContent} value={content}/>
-      <button style={{ marginTop: "10px" }}>Create post</button>
-    </form>
+      <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'flex-end' }}>
+        <Button
+          variant="contained"
+          component="label"
+          style={{ textTransform: 'none', fontWeight: 'bold', backgroundColor: '#6f6f6f', width: 400 }}
+        >
+          {fileName || "Upload File"}
+          <input
+            style={{ display: "none"}}
+            type="file"
+            hidden
+            onChange={handleFileChange}
+          />
+        </Button>
+        {filePreview && (
+          <Box mt={2} style={{ alignItems: 'flex-end' }}>
+            <img src={filePreview} alt="Preview" style={{ maxWidth: '100%', maxHeight: 100, alignItems: 'flex-end' }} />
+          </Box>
+        )}
+      </Box>
+      <Editor onChange={setContent} value={content} />
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Button
+          variant="contained"
+          type="submit"
+          style={{ textTransform: 'none', fontWeight: 'bold', maxWidth: 150, borderRadius: 5, backgroundColor: '#00d410', fontSize: 16 }}
+          sx={{
+            '&:hover': {
+              backgroundColor: '#32cd32',
+            },
+          }}
+        >
+          Create post
+        </Button>
+      </Box>
+    </Box>
   );
 }
