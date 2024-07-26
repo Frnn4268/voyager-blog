4853e78108e117ff4fb3a3a79c78ec7c4ab44051
Frnn4268 <appstutorialesy@gmail.com>
2024-07-25 18:14:47 -0600

    Adding new styles in App.css and modifying PostPage.jsx

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,3 +1,4 @@
+import { formatISO9075 } from "date-fns/formatISO9075";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -17,14 +18,16 @@ export default function PostPage() {
 
   return (
     <div className="post-page">
+      <h1>{postInfo.title}</h1>
+      <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+      <div className="author">by @{postInfo.author.username}</div>
       <div className="image">
         <img
           src={`${process.env.REACT_APP_API_UPLOAD}${postInfo.cover}`}
           alt=""
         />
       </div>
-      <h1>{postInfo.title}</h1>
-      <div dangerouslySetInnerHTML={{__html:postInfo.content}}/>
+      <div dangerouslySetInnerHTML={{ __html: postInfo.content }} />
     </div>
   );
 }
